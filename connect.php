19e945b8c96191854b2478c112d095a5135c3585
tinoxn <niyonshutivalentin@gmail.com>
2019-08-02 09:13:10 +0100

    php script

---
+++ connect.php
@@ -0,0 +1,6 @@
+<?php
+mysqli_connect('localhost','root','','library')or die("wapi");
+// $con=mysqli_connect('db4free.net','webdev','1234webdev','webdev')or die("wapi");
+?>
+<!-- 
+$con=//mysqli_connect('db4free.net','webdev','1234webdev','webdev')or die("wapi"); -->
