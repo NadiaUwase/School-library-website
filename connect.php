abd77dd919f331a3ccec72a97c065d512a6ddf09
tinoxn <niyonshutivalentin@gmail.com>
2019-08-02 10:27:23 +0100

    db

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
-$con=mysqli_connect('localhost','root','','library')or die("wapi");
-// $con=mysqli_connect('db4free.net','webdev','1234webdev','webdev')or die("wapi");
+// $con=mysqli_connect('localhost','root','','library')or die("wapi");
+$con=mysqli_connect('db4free.net','webdevlibrary1','privacupolicy99','webdevlibrary1')or die("wapi");
 ?>
 <!-- 
 $con=//mysqli_connect('db4free.net','webdev','1234webdev','webdev')or die("wapi"); -->
